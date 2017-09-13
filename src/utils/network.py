c5e2b2e3b59831d581b807e544f8b31581929e93
Eric Zhao <ericlzhao28@gmail.com>
2017-09-13 16:55:02 -0700

    Added networking module

---
+++ src/utils/network.py
@@ -0,0 +1,12 @@
+import requests
+
+def download_file(url, filename):
+  '''
+  Generic function to stream-download a large file
+  '''
+  r = requests.get(url, stream=True)
+  with open(filename, 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+      if chunk:
+        f.write(chunk)
+  return filename
