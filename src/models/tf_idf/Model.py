b909e7f5de48dd3f3ca3a2c38d1055f3b9854da6
Eric Zhao <ericlzhao28@gmail.com>
2017-09-20 17:10:26 -0700

    Added tf_idf models and scripts

--- src/models/tf_idf/Model.py
+++ src/models/tf_idf/Model.py
@@ -1,13 +1,27 @@
-class Model():
-  def __init__(self, relations):
-    pass
+import logging
+from gensim.models.tfidfmodel import TfidfModel
+from gensim.corpora import Dictionary
 
-  def train(self):
-    pass
+from . import config
+from . import Corpus as DefaultCorpus
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class Model():
+  def train(self, corpus):
+    corpus.dictionary = Dictionary(corpus.get_texts())
+    self.tfidf = TfidfModel(corpus)
+    corpus.dictionary.save(config.MODEL_DIR + config.DICT_SAVE)
+    self.tfidf.save(config.MODEL_DIR + config.TFIDF_SAVE)
+    return corpus
 
   def load(self):
-    pass
+    corpus = DefaultCorpus.Corpus()
+    corpus.dictionary = Dictionary.load(config.MODEL_DIR + config.DICT_SAVE)
+    self.tfidf = TfidfModel.load(config.MODEL_DIR + config.TFIDF_SAVE)
+    return corpus, self.tfidf
 
-  def score(self, subword, word):
-    pass
+  def score(self, word):
+    return self.tfidf[word]
 
