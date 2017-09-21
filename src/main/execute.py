6436a6fc42e8195187e15a27c86c3a9920fc824f
Eric Zhao <ericlzhao28@gmail.com>
2017-09-20 17:14:30 -0700

    Added cleaning and revised pipeline for new tf models

--- src/main/execute.py
+++ src/main/execute.py
@@ -1,9 +1,10 @@
 from ..datasets import SimpleQuestions
 from ..models.tf_idf import TF_IDF
-from ..models.tensorflow_query_structuring import EntityDetector, RelationshipClassifier
+from ..models.tensorflow_query_structuring import Parser
 
 from .entity_linking import match_entities, score_entities
 from .answer_selection import gather_candidate_answers
+from ..utils import cleaning
 
 
 def initialize():
@@ -14,24 +15,20 @@ def initialize():
   tf_idf_model.load()
   score_entities(graph, tf_idf_model)
 
-  entity_detector = EntityDetector()
-  entity_detector.load()
-
-  relationship_clasifier = RelationshipClassifier()
-  relationship_clasifier.load()
+  local_parser = Parser()
+  local_parser.build_model()
 
   def execute(query):
-    entity_inds = entity_detector.predict(query)
-    full_entity = "".join([query[i] for i in entity_inds])
-    predicted_relationship = relationship_clasifier.predict(query)
+    query = [cleaning.clean_name(x) for x in query.split(" ")]
+    entity_inds = local_parser.predict_entities(query)
+    full_entity = " ".join([query[i] for i in entity_inds])
+    predicted_relationship = local_parser.predict_relationship(query)
 
     base_entity_candidates = match_entities(graph, full_entity)
-    # We assume the largest phrase match is the most likely
 
     candidate_answers = []
     for base_entity in base_entity_candidates:
       candidate_answers += gather_candidate_answers(base_entity, predicted_relationship, graph)
-
     return candidate_answers[0]
 
   return execute
@@ -39,13 +36,10 @@ def initialize():
 
 def train():
   graph = SimpleQuestions.create_graph()
-   
+
   tf_idf_corpus = TF_IDF.Corpus(graph.get_all_relations)
   tf_idf_model = TF_IDF.Model()
   tf_idf_model.train(tf_idf_corpus)
 
-  entity_detector = EntityDetector()
-  entity_detector.train()
-
-  relationship_clasifier = RelationshipClassifier()
-  relationship_clasifier.train()
+  local_parser = Parser()
+  local_parser.train()
