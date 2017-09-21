b42a2d22d66255e54d987d880b902a99905901c5
Eric Zhao <ericlzhao28@gmail.com>
2017-09-21 16:44:20 -0700

    Finished main utils tests

--- tests/main/test_main_utils.py
+++ tests/main/test_main_utils.py
@@ -6,17 +6,29 @@ def test_build_alts():
 
 
 def test_build_candidate_entities():
-  def __get_entity():
-    pass
-  assert(True is utils.build_candidate_entities(__get_entity, "hello i like food"))
+  def __get_entity(entity):
+    return entity in ["i", "food", "like food"]
+  assert(["i", "food", "like food"] == utils.build_candidate_entities(__get_entity, "hello i like food"))
 
 
 def test_score_entities():
-  def __update_entity():
-    pass
+  updates = {}
 
-  def __tf_idf_scorer():
-    pass
+  def __update_entity(entity, key, value, create_if_none=False):
+    updates[entity] = (key, value)
 
-  assert(True is utils.build_candidate_entities(__update_entity, __tf_idf_scorer, "hello i like food"))
+  def __tf_idf_scorer(entity, doc):
+    denominator = 100.0 if doc == "hello i like food" else 2.0
+    if entity in ["hello", "i", "like", "food", "like food"]:
+      numerator = 50.0
+    else:
+      numerator = 5.0
+    return numerator / denominator
+
+  utils.build_candidate_entities(__update_entity, __tf_idf_scorer, ["hello i like food"])
+
+  assert(updates['hello'] == ("tfidf", 0.5))
+  assert(updates['i'] == ("tfidf", 0.5))
+  assert(updates['like'] == ("tfidf", 0.5))
+  assert(updates['food'] == ("tfidf", 0.5))
 
