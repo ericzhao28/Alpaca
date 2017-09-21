6e6925ba49180a2118a9cda258b4f6a294593f35
Eric Zhao <ericlzhao28@gmail.com>
2017-09-21 15:16:49 -0700

    Finished tests for graph layer

--- tests/graph_layer/test_graph_layer.py
+++ tests/graph_layer/test_graph_layer.py
@@ -11,14 +11,24 @@ def test_graph():
   assert(graph.get_entity("hugs")['test'] == "True")
   graph.add_relationship("hugs", "chocolate", "ideal")
 
+  assert(len(graph.get_all_relations()) == 1)
+  assert(len(graph.entity_names) == 2)
+  assert(graph.get_parents("chocolate") == "hugs")
+  assert(graph.get_parents("chocolate", "ideal") == "hugs")
+  assert(graph.get_parents("chocolate", "ideal2") == False)
+  assert(graph.get_childs("hugs") == "chocolate")
+  assert(graph.get_childs("hugs", "ideal") == "chocolate")
+  assert(graph.get_childs("hugs", "ideal2") == False)
+  assert(graph.get_entity("chocolate2") == False)
+
   graph.import_edge("hugs2", "ideal2", "chocolate2")
   graph.update_entity("chocolate2", "test", "True")
   graph.update_entity("hugs2", "test", "True")
   assert(graph.get_entity("chocolate2")['test'] == "True")
   assert(graph.get_entity("hugs2")['test'] == "True")
 
-  for rel in graph.get_all_relations():
-    assert(rel == "")
+  assert(len(graph.get_all_relations()) == 2)
+  assert(len(graph.entity_names) == 4)
 
   graph.wipe_tests()
 
--- tests/graph_layer/test_graph_load.py
+++ tests/graph_layer/test_graph_load.py
@@ -1,7 +1,32 @@
+from ...src.graph_layer import load
+from . import config
+
+
 def test_create_graph():
-  pass
+  graph = load.create_graph(config.DATA_DIR + config.DUMP_SAVE)
+  assert(graph.get_all_relations() == ["is", "not"])
+  assert(graph.entity_names() == ["chocolate", "hugs", "people", "good", "bad"])
+  assert(graph.get_childs("chocolate") == ["good", "bad"])
+  assert(graph.get_childs("chocolate", "not") == ["bad"])
+  graph.update_entity("chocolate", "test", "True")
+  graph.update_entity("hugs", "test", "True")
+  graph.update_entity("people", "test", "True")
+  graph.update_entity("good", "test", "True")
+  graph.update_entity("bad", "test", "True")
+  graph.wipe_tests()
 
 
 def test_load_graph():
-  pass
+  load.create_graph(config.DATA_DIR + config.DUMP_SAVE)
+  graph = load.load_graph()
+  assert(graph.get_all_relations() == ["is", "not"])
+  assert(graph.entity_names() == ["chocolate", "hugs", "people", "good", "bad"])
+  assert(graph.get_childs("chocolate") == ["good", "bad"])
+  assert(graph.get_childs("chocolate", "not") == ["bad"])
+  graph.update_entity("chocolate", "test", "True")
+  graph.update_entity("hugs", "test", "True")
+  graph.update_entity("people", "test", "True")
+  graph.update_entity("good", "test", "True")
+  graph.update_entity("bad", "test", "True")
+  graph.wipe_tests()
 
